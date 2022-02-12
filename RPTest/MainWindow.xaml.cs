f7381089647fdd7d0d1e2abf6f7449426a96903c
Заид Мингалиев <zzmingaliev@outlook.com>
2022-02-12 21:21:56 +0300

    Добавьте файлы проекта.

---
+++ RPTest/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace RPTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            var application = new Word.Application();
+            Word.Document document = new Word.Document();
+
+            InsertSimplrRangeBoldCenterAlignment(document, "федеральное государственное бюджетное образовательное учреждение высшего образования «Казанский национальный исследовательский технический университет им. А.Н.Туполева - КАИ» (КНИТУ - КАИ)" + "\n");
+
+            InsertSimpleRangeCenterAlignment(document, "Институт компьютерных технологий и защиты информации" + "\n\n" + "Отделение СПО ИКТЗИ «Колледж информационных технологий»" + "\n");
+
+            application.Visible = true;
+            document.SaveAs2(@"C:\Users\zzmin\source\repos\RPTest\RPTest\doc1.docx");
+            document.Close();
+        }
+        /// <summary>
+        /// Добавление параграфа текста (жирное начертание, выравнивание по центру)
+        /// </summary>
+        /// <param name="document">Ссылка на редактируемый документ</param>
+        /// <param name="insertedText">Вставляемый текст</param>
+        private static void InsertSimplrRangeBoldCenterAlignment(Word.Document document, string insertedText)
+        {
+            Word.Paragraph paragraph = document.Paragraphs.Add();
+            Word.Range range = paragraph.Range;
+            range.Font.Color = Word.WdColor.wdColorBlack;
+            range.Bold = 1;
+            range.Font.Size = 14;
+            range.Font.Name = "Times New Roman";
+            range.Text = insertedText;
+            range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            range.InsertParagraphAfter();
+        }
+        /// <summary>
+        /// Добавление параграфа текста (обычное начертание, выравнивание по центру)
+        /// </summary>
+        /// <param name="document">Ссылка на редактируемый документ</param>
+        /// <param name="insertedText">Вставляемый текст</param>
+        private static void InsertSimpleRangeCenterAlignment(Word.Document document, string insertedText)
+        {
+            Word.Paragraph paragraph = document.Paragraphs.Add();
+            Word.Range range = paragraph.Range;
+            range.Font.Color = Word.WdColor.wdColorBlack;
+            range.Font.Size = 14;
+            range.Font.Name = "Times New Roman";
+            range.Text = insertedText;
+            range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+            range.InsertParagraphAfter();
+        }
+    }
+}
